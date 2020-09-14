33449fd0ba1d34630c18fbed4fb464e6ca62ec76
johnvamba <johnvamba@gmail.com>
2020-09-14 20:28:01 +0800

    Config: database configure for heroku

--- config/database.php
+++ config/database.php
@@ -40,6 +40,7 @@ return [
         ],
 
         'mysql' => [
+            'url' => env('DATABASE_URL', ''),
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
