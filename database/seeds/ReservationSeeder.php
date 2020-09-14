5828a66260ebc02932d04c18082dc97b1785dda1
johnvamba <johnvamba@gmail.com>
2020-09-14 21:41:42 +0800

    Update: Reservation seeder using simple create

--- database/seeds/ReservationSeeder.php
+++ database/seeds/ReservationSeeder.php
@@ -11,6 +11,25 @@ class ReservationSeeder extends Seeder
      */
     public function run()
     {
-        \factory(Space::class, 15)->create();
+        // \factory(Space::class, 15)->create();
+        Space::create([
+        	'name' => 'Table 1',
+        	'description' => "Top right corner; max 5 pax"
+        ]);
+
+        Space::create([
+        	'name' => 'Table 2',
+        	'description' => "Top left near door; max 3 pax"
+        ]);
+
+        Space::create([
+        	'name' => 'Table 3',
+        	'description' => "Bottom left of counter; max 2 pax"
+        ]);
+
+        Space::create([
+        	'name' => 'Table 3',
+        	'description' => "Bottom right of counter; max 2 pax"
+        ]);
     }
 }
